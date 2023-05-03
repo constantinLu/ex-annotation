fb6058c4951c730dd29d5fb03f9de46c5c273659
Constantin Lungu <lunguucatalin@gmail.com>
2023-05-03 16:10:42 +0300

    first module

---
+++ src/main/java/com/example/annotationex/AnnotationExApplication.java
@@ -0,0 +1,13 @@
+package com.example.annotationex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnotationExApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnnotationExApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/annotationex/AnnotationExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.annotationex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnotationExApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
